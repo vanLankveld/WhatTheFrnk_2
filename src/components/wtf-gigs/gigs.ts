d5a2b22677cdb9c67008099bd9a34847bbfb4c64
Guus van Lankveld <info@guusvanlankveld.nl>
2023-09-05 14:08:12 +0200

    gig agenda update

--- src/components/wtf-gigs/gigs.ts
+++ src/components/wtf-gigs/gigs.ts
@@ -59,6 +59,11 @@ export var gigs: Gig[] = [
     new Gig({ date: '2023-07-16', city: 'Blerick', time: "18:00", venue: 'Bli&euml;rock', website: "https://www.stichtingb4music.nl/blierock" }),
     new Gig({ date: '2023-10-28', city: 'Gemert', time: "20:00", venue: 'De Bunker', website: "https://www.bunkergemert.nl/" }),
     new Gig({ date: '2023-12-12', city: 'Zwolle', venue: 'Hedon', website: "https://www.hedon-zwolle.nl/" }),
+    new Gig({ date: '2023-10-01', city: 'Alkmaar', venue: 'Eetcaf&eacute; Pluim', time: "17:00", website: "https://www.eetcafepluim.nl/" }),
+    new Gig({ date: '2023-10-07', city: 'Brugge', venue: '27b Flat', website: "https://www.27bflat.be/nl/" }),
+    new Gig({ date: '2023-10-08', city: 'Nieuw Vennep', venue: 'Finale Dutch Blues Challenge', website: "https://www.dutchbluesfoundation.nl/dutch-blues-challenge" }),
+    new Gig({ date: '2023-10-15', city: 'Bergeijk', venue: 'Kempen TV', website: "https://www.kempentv.nl/" }),
+    new Gig({ date: '2024-01-27', city: 'Assen', venue: 'Asser Bluesdagen', website: "https://bluesassen.nl/" }),
 ]
 
 gigs.sort((a, b) => {
